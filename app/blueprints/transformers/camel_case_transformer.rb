35d98b3d130fe3f78c03f87c1d683ce5a1123cca
= <paulgdan@gmail.com>
2025-10-24 17:58:24 -0400

    fixes

--- app/blueprints/transformers/camel_case_transformer.rb
+++ app/blueprints/transformers/camel_case_transformer.rb
@@ -1,9 +1,7 @@
-module Blueprinter
-  module Transformers
-    class CamelCaseTransformer
-      def self.call(hash)
-        hash.transform_keys! { |key| key.to_s.camelize(:lower).to_sym }
-      end
+module Transformers
+  class CamelCaseTransformer
+    def self.call(hash)
+      hash.transform_keys! { |key| key.to_s.camelize(:lower).to_sym }
     end
   end
 end
