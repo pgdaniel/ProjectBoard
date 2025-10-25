9fa465c1a272255d07f6e87aec76f25f8fe90e5c
= <paulgdan@gmail.com>
2025-10-25 09:27:58 -0400

    Disable CORS in production, let Caddy handle it

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', headers: :any, methods: %i[get post put patch delete options]
+# CORS is handled by Caddy reverse proxy in production
+# In development, allow all origins for local testing
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: %i[get post put patch delete options]
+    end
   end
 end
